7a25c22a167764f54dfa9d05e018850e85dbf9b6
NomotoEriko <nomoto.eriko@ist.osaka-u.ac.jp>
2019-06-22 00:26:12 +0900

    :zap: on memory

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     login_name  VARCHAR(128) NOT NULL,
     pass_hash   VARCHAR(128) NOT NULL,
     UNIQUE KEY login_name_uniq (login_name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS events (
     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS events (
     public_fg   TINYINT(1)       NOT NULL,
     closed_fg   TINYINT(1)       NOT NULL,
     price       INTEGER UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS sheets (
     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS sheets (
     num         INTEGER UNSIGNED NOT NULL,
     price       INTEGER UNSIGNED NOT NULL,
     UNIQUE KEY rank_num_uniq (`rank`, num)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS reservations (
     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS reservations (
     reserved_at DATETIME(6)      NOT NULL,
     canceled_at DATETIME(6)      DEFAULT NULL,
     KEY event_id_and_sheet_id_idx (event_id, sheet_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS administrators (
     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -38,4 +38,4 @@ CREATE TABLE IF NOT EXISTS administrators (
     login_name  VARCHAR(128) NOT NULL,
     pass_hash   VARCHAR(128) NOT NULL,
     UNIQUE KEY login_name_uniq (login_name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4;
--- webapp/python/run_app.sh
+++ webapp/python/run_app.sh
@@ -3,9 +3,11 @@ ROOT=/root/isucon8-qualify
 APP=${ROOT}/webapp/python
 
 /etc/init.d/mysql start
+echo start
 mysql -uroot -e"CREATE USER isucon@'%' IDENTIFIED BY 'isucon';"
 mysql -uroot -e"GRANT ALL on torb.* TO isucon@'%';"
 mysql -uroot -e"CREATE USER isucon@'localhost' IDENTIFIED BY 'isucon';"
 mysql -uroot -e"GRANT ALL on torb.* TO isucon@'localhost';"
+mysql -uroot -e"SET GLOBAL max_heap_table_size = 1024 * 1024 * 800;"
 ${ROOT}/db/init.sh
 DB_DATABASE=torb DB_HOST=127.0.0.1 DB_PORT=3306 DB_USER=isucon DB_PASS=isucon python ${APP}/app.py
